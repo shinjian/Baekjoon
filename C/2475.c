b68c24f9f288d4316fb8647d53bf876d3fc9321b
jian1130 <jian11300@gmail.com>
2022-02-09 18:02:36 +0900

    2475 - 검증수
    Baekjoon Online Judge
    https://www.acmicpc.net/problem/2475

---
+++ C/2475.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int num, result = 0;
+    for(int i = 0; i <= 4; i++){
+        scanf("%d", &num);
+        result += num * num;
+    }
+    printf("%d", result % 10);
+}
