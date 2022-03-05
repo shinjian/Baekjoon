d2f43ed0b13cf8f25c189ab81f931d014edfdaa1
jian1130 <jian11300@gmail.com>
2022-03-05 21:12:14 +0900

    2445 - 별 찍기 - 8
    Baekjoon Online Judge
    https://www.acmicpc.net/problem/2445

---
+++ Python/2445.py
@@ -0,0 +1,6 @@
+# 별 찍기 - 8 - https://www.acmicpc.net/problem/2445
+n = int(input())
+for i in range(1, n):
+    print('*'*i + ' '*((n-i)*2) + '*'*i)
+for i in range(n, 0, -1):
+    print('*'*i + ' '*((n-i)*2) + '*'*i)
